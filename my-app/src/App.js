b7a4195a9e27bf65d7f039f24a136792656815ee
Abhinav <abhinavsingh9990604@gmail.com>
2025-10-22 17:35:36 +0530

    First React App

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 id='heading'>This is My First React App</h1>
+    </div>
+  );
+}
+
+export default App;
