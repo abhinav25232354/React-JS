f8843d162f79cd49ad77c67a7547c80d0751a1a1
Abhinav <abhinavsingh9990604@gmail.com>
2026-03-21 07:33:34 +0530

    feat: initialize project-1 with React setup, including essential files and dependencies
    - Added package.json with dependencies for React, testing libraries, and scripts.
    - Created public assets: favicon.ico, index.html, logo192.png, logo512.png, manifest.json, and robots.txt.
    - Implemented basic App component with a Navbar and styling in App.css.
    - Set up testing framework with App.test.js and Jest DOM matchers.
    - Included reportWebVitals for performance measurement.
    - Added Navbar component for navigation.

--- props-proptypes/src/App.js
+++ props-proptypes/src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
+// Making our First Component in React
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +22,8 @@ function App() {
         </a>
       </header>
     </div>
+    <Navbar/>
+    </>
   );
 }
 
